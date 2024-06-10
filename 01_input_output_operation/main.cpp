d06fec6fb49852246166f28db167b9d690663e5b
kenjayev <kenjayevazizbek2oo4@gmail.com>
2024-06-11 00:13:48 +0500

    first project

---
+++ 01_input_output_operation/main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    /*
+    float a, v, s;
+    cout << "Kubning qirrasi a="; cin >> a;
+    v = a*a*a;
+    s = 6*a*a;
+    cout << "Hajmi v = " << v << endl;
+    cout << "Yuzasi s = " << s << endl;
+    */
+
+    /*
+    float a, b, c, s;
+    cout << "Katet a = "; cin >> a;
+    cout << "Katet b = "; cin >> b;
+    c = sqrt(pow(a, 2) + pow(b, 2));
+    s = a * b /2;
+    cout << "Gipatenuza c = " << c << endl;
+    cout << "Yuza s = " << s << endl;
+    */
+
+    /*
+    float a, b, arif, geo;
+    cout << "1-sonni krting a = "; cin >> a;
+    cout << "2-sonni krting b = "; cin >> b;
+    arif = (a + b) / 2;
+    geo = sqrt(a*b);
+    cout << arif << " " << geo << endl;
+    */
+    /*
+    int const g = 10;
+    float h, t;
+    cout << "Banadlikni krtiting h = "; cin >> h;
+    t = sqrt(2 * h / g);
+    cout << t << endl;
+    */
+
+    /*
+    float t1, t2, t, v1, v2, v;
+    cout << "1 - suv t1 va v1 ni kriting "; cin >> t1 >> v1;
+    cout << "2 - suv t2 va v2 ni kriting "; cin >> t2 >> v2;
+    v = v1 + v2;
+    t = ((v1 + v2) * t2 + v1*t1) / (2 * v1 + v2);
+    cout << v << " " << t << endl;
+    */
+    float x, y, s;
+    cout << "x va y kriting: " ; cin >> x >> y;
+    s = (fabs(x) - fabs(y)) / (1 - fabs(x*y));
+    cout << s << endl;
+
+
+    return 0;
+}
