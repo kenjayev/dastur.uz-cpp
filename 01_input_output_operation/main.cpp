3496e7eb3bf826f5cc96599534fc9daa67803b7b
kenjayev <kenjayevazizbek2oo4@gmail.com>
2024-06-11 01:04:46 +0500

    1-10 masala

--- 01_input_output_operation/main.cpp
+++ 01_input_output_operation/main.cpp
@@ -54,7 +54,78 @@ int main()
     s = (fabs(x) - fabs(y)) / (1 - fabs(x*y));
     cout << s << endl;
     */
+    /**
+    float a, p, s;
+    cin >> a;
+    p = 4 * a;
+    s = pow(a, 2);
+    cout << "p = " << p << endl;
+    cout << "s = " << s << endl;
+    */
 
+    /* 2- masala */
+    /**
+    float a, b, p, s;
+    cout << "a va b: "; cin >> a >> b;
+    p = 2 * (a + b);
+    s = a * b;
+    cout << p << " " << s << endl;
+    */
+
+
+    /* 3- masala */
+    /**
+    float const p = 3.14;
+    float d, l;
+    cout << "d: "; cin >> d;
+    l = p * d;
+    cout << l << endl;
+    */
+
+    /* 4- masala */
+    /**
+    float a, v, s;
+    cout << "a: "; cin >> a;
+    v = pow(a, 3);
+    s = 6 * pow(a, 2);
+    cout << v << " " << s << endl;
+    */
+
+    /* 6-masala */
+    /**
+    float a, b, c, v, s;
+    cout << "a, b, c: "; cin >> a >> b >> c;
+    v = a * b * c;
+    s = 2(a*b + a*c + b*c);
+    cout << v << " " << s << endl;
+    */
+
+    /* 7-masala */ /**
+    float const p = 3.14;
+    float r, l, s;
+    cout << "r: "; cin >> r;
+    l = 2 * p * r;
+    s = p*pow(r, 2);
+    cout << l << " " << s << endl;
+    */
+
+    /* 8-9-masala */ /**
+    float a, b, arx, geo;
+    cout << "a, b: "; cin >> a >> b;
+    arx = (a+ b)/2;
+    geo = sqrt(a*b)
+    cout << arx << endl;
+    */
+
+     /* 10-masala */ /**
+     float a, b, kop, bol, qa, qb;
+     cout << "a, b: "; cin >> a >> b;
+     kop = a * b;
+     bol = a / b;
+     qa = a*a;
+     qb = b * b;
+     cout << kop << " " << bol << " " << qa << " " << qb << endl;
+     */
 
     return 0;
 }
